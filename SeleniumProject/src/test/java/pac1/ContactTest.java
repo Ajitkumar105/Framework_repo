d785b7e14939706acd38686fc04b80d7685d92cc
Ajitkumar105 <Ajit22062@gmail.com>
2025-04-05 11:07:23 +0530

    first push

---
+++ SeleniumProject/src/test/java/pac1/ContactTest.java
@@ -0,0 +1,26 @@
+package pac1;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	@Test
+	public void createContactTest()
+	{   
+		String URL=System.getProperty("url");
+		String BROWESER=System.getProperty("browser");
+		String USERNAME=System.getProperty("username");
+		String PASSWORD=System.getProperty("password");
+		System.out.println(URL);
+		System.out.println(BROWESER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+		System.out.println("execute createContactTes");
+	}
+	
+    @Test
+	public void modifycreateContactTest()
+	{
+		System.out.println("execute modifycreateContactTes");
+	}
+
+}
